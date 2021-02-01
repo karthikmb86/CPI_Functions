796048f80def2aecab8db18a08f3ab9a28299f54
karthikmb86 <47756794+karthikmb86@users.noreply.github.com>
2021-02-01 12:49:32 +1100

    Add files via upload

---
+++ GroovyTestUtility/src/com/sap/gateway/ip/core/customdev/util/Message.java
@@ -0,0 +1,22 @@
+package com.sap.gateway.ip.core.customdev.util;
+
+public class Message {
+
+    String body;
+
+    public String getBody(){
+           return body;
+    }
+
+    public void setBody(String Body){
+        this.body = Body;
+    }
+
+    public void testGit(String Body){
+        this.body = Body;
+    }
+
+    public void setProperty(String Key, String Val){
+
+    }
+}
---
+++ GroovyTestUtility/src/com/sap/it/api/mapping/Output.java
@@ -0,0 +1,8 @@
+package com.sap.it.api.mapping;
+
+public class Output {
+
+    public void addValue(String value){
+        //add code
+    }
+}
