6bea169d55319befd9ca367d8c5ae01f37fd9a06
karthikmb86 <47756794+karthikmb86@users.noreply.github.com>
2021-02-01 16:26:21 +1100

    sda

---
+++ GroovyTestUtility/src/com/sap/gateway/ip/core/customdev/util/Message.groovy
@@ -0,0 +1,23 @@
+package com.sap.gateway.ip.core.customdev.util;
+import java.util.*;
+
+public class Message {
+
+    String body;
+
+    public String getBody(){
+        return body;
+    }
+
+    public void setBody(String Body){
+        this.body = Body;
+    }
+
+    public void testGit2(String Body){
+        this.body = Body;
+    }
+
+    public void setProperty(String Key, String Val){
+
+    }
+}
