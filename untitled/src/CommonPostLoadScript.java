22ddb0667f3c6b6e15c5caa7059857ebd6cbbd07
karthikmb86 <47756794+karthikmb86@users.noreply.github.com>
2021-02-01 12:28:25 +1100

    Delete untitled directory

--- untitled/src/CommonPostLoadScript.java
+++
@@ -1,8 +0,0 @@
-# Post Load script
-        print('INFO - Remember successful load date: '|| $G_LOAD_DATE);
-        save_data(job_name() || '-LOAD_DATE',to_char($G_LOAD_DATE,'yyyy-mm-dd hh24:mi:ss'));
-        if ($G_STANDARD_LOAD = 'yes')
-        begin
-        print('INFO - Remember successful load date with standard settings: '|| $G_LOAD_DATE);
-        save_data(job_name() || '-STANDARD_LOAD_DATE',to_char($G_LOAD_DATE,'yyyy-mm-dd hh24:mi:ss'));
-        end
